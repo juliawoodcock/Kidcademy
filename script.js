903f40e37522c2781577a1cd4504a4718202f55a
Julia Woodcock <56485868+juliawoodcock@users.noreply.github.com>
2020-08-22 13:41:08 -0400

    updated all buttons

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  function YesFunction() {
+function YesFunction() {
 
   }
 
-  function NoFunction(){
+function NoFunction(){
+
+  }
+
+function SquareFunction(){
       
-  }
+    }
+
+function CircleFunction(){
+      
+    }
+
+function TriangleFunction(){
+      
+    }
+
+function DiamondFunction(){
+      
+    }
